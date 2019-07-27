771f13662a8ab23a0b1a8ce2777a44d3b26871f1
Dennis P Kamah <dkamah913@gmail.com>
2019-07-27 10:40:50 +0700

    Initiate auth

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::resource('vehicle_type', 'VehicleTypeController');
