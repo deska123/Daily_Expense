f58dc210d75419a05ddad34808af8e8567f431fc
Dennis P Kamah <dkamah913@gmail.com>
2019-08-03 22:40:52 +0700

    Change into blade style

--- resources/views/vehicle_type/index.blade.php
+++ resources/views/vehicle_type/index.blade.php
@@ -15,45 +15,45 @@
           </tr>
         </thead>
         <tbody>
-          <?php
+          @php
             if($data['list']->onFirstPage()) {
               $a = 1;
             } else {
               $a = (($data['list']->currentPage() - 1) * $data['list']->perPage()) + 1;
             }
-          ?>
-          <?php foreach($data['list'] as $vehicleType): ?>
-          <tr>
-            <td>{{ $a }}</td>
-            <td>{{ $vehicleType->type }}</td>
-            <td>
-              <a href="{{url('vehicle_type/' . $vehicleType -> id)}}" class="btn btn-secondary btn-md">Details</a>
-              <a href="{{url('vehicle_type/' . $vehicleType -> id . '/edit')}}" class="btn btn-info btn-md">Edit</a>
-              <button type="button" class="btn btn-danger btn-md" data-toggle="modal" data-target="#deleteVehicleTypeModal_{{$vehicleType -> id}}">Delete</button>
-            </td>
-            <div id="deleteVehicleTypeModal_{{$vehicleType -> id}}" class="modal fade" role="dialog">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h4 class="modal-title">Delete Confirmation</h4>
-                  </div>
-                  <div class="modal-body">
-                    <p>Are you sure you want to delete this data ?</p>
-                  </div>
-                  <div class="modal-footer">
-                      <form action="{{ url('vehicle_type/' . $vehicleType -> id) }}" method="POST">
-                      @method('DELETE')
-                      @csrf
-                      <button type="submit" class="btn btn-primary">Yes</button>
-                    </form>
-                    <button type="button" class="btn btn-warning" data-dismiss="modal">No</button>
+          @endphp
+          @foreach ($data['list'] as $vehicleType)
+            <tr>
+              <td>{{ $a }}</td>
+              <td>{{ $vehicleType->type }}</td>
+              <td>
+                <a href="{{url('vehicle_type/' . $vehicleType -> id)}}" class="btn btn-secondary btn-md">Details</a>
+                <a href="{{url('vehicle_type/' . $vehicleType -> id . '/edit')}}" class="btn btn-info btn-md">Edit</a>
+                <button type="button" class="btn btn-danger btn-md" data-toggle="modal" data-target="#deleteVehicleTypeModal_{{$vehicleType -> id}}">Delete</button>
+              </td>
+              <div id="deleteVehicleTypeModal_{{$vehicleType -> id}}" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h4 class="modal-title">Delete Confirmation</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p>Are you sure you want to delete this data ?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <form action="{{ url('vehicle_type/' . $vehicleType -> id) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-primary">Yes</button>
+                      </form>
+                      <button type="button" class="btn btn-warning" data-dismiss="modal">No</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </tr>
-          <?php $a++ ?>
-          <?php endforeach ?>
+            </tr>
+            @php $a++; @endphp
+          @endforeach
         </tbody>
       </table>
     @else
